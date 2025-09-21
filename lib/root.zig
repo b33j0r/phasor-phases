d19e37aeb1558fac36b2a6e2fc2f613617c600ae
Brian Jorgensen <brian.jorgensen@gmail.com>
2025-09-21 12:45:45 -0600

    remove unused StateMachine (it was a prototype for solving the hierarchical phase problem)

--- lib/root.zig
+++ lib/root.zig
@@ -6,11 +6,7 @@ pub const PhaseContext = @import("PhaseContext.zig");
 pub const phase_context_stack_mod = @import("phase_context_stack.zig");
 pub const PhaseContextStack = phase_context_stack_mod.PhaseContextStack;
 
-const state_machine_mod = @import("state_machine.zig");
-pub const StateMachine = state_machine_mod.StateMachine;
-
 test "ref all decls" {
     _ = PhasePlugin;
     _ = PhaseContext;
-    _ = StateMachine;
 }
